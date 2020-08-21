ef395a8bd55b710885085117dfb3e53227beb467
Rokas Atminas <rokasatminas@gmail.com>
2020-08-22 02:33:00 +0300

    created project with spring initializr

---
+++ src/main/java/com/blog/reviewwebsite/ReviewWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.blog.reviewwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/reviewwebsite/ReviewWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.reviewwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
