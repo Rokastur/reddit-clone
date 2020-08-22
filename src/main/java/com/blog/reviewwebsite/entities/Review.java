61ce0bfd560b1821bfb6805069d547ff758c01be
Rokas Atminas <rokasatminas@gmail.com>
2020-08-22 03:36:37 +0300

    created review entity

---
+++ src/main/java/com/blog/reviewwebsite/entities/Review.java
@@ -0,0 +1,28 @@
+package com.blog.reviewwebsite.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "Review")
+public class Review {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "review_id")
+    private Long id;
+
+    @Column(name="author")
+    private String author;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "rating")
+    private int rating;
+
+    @Column(name = "review", columnDefinition = "text")
+    private String review;
+}
