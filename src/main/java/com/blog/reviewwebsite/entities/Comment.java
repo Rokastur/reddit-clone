c9924e595bf019da5d1523845ad455cbb217d0b2
Rokas Atminas <rokasatminas@gmail.com>
2020-09-20 20:46:47 +0300

    Now comments display and link to correct comment author. Removed unused constructors in Comment.java

--- src/main/java/com/blog/reviewwebsite/entities/Comment.java
+++ src/main/java/com/blog/reviewwebsite/entities/Comment.java
@@ -10,14 +10,6 @@ import javax.validation.constraints.NotBlank;
 @Table(name = "comments")
 public class Comment {
 
-    public Comment(User user, Review review) {
-        this.user = user;
-        this.review = review;
-    }
-
-    public Comment() {
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
