b88dcdc835288b12698917df406f3151af03a303
Rokas Atminas <rokasatminas@gmail.com>
2020-12-13 22:46:00 +0200

    creating a new comment will automatically upvote it (creates new score entity with rating type upvote to the new comment)

--- src/main/java/com/blog/reviewwebsite/controller/CommentController.java
+++ src/main/java/com/blog/reviewwebsite/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.blog.reviewwebsite.controller;
 import com.blog.reviewwebsite.entities.Comment;
 import com.blog.reviewwebsite.entities.User;
 import com.blog.reviewwebsite.services.CommentService;
+import com.blog.reviewwebsite.services.ScoreService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -15,15 +16,19 @@ import javax.validation.Valid;
 public class CommentController {
 
     private CommentService commentService;
+    private ScoreService scoreService;
 
-    public CommentController(CommentService commentService) {
+    public CommentController(CommentService commentService, ScoreService scoreService) {
         this.commentService = commentService;
+        this.scoreService = scoreService;
     }
 
     @PostMapping("/submit/{id}")
     public String submitComment(@Valid @ModelAttribute("newComment") Comment comment, BindingResult result, @PathVariable Long id, @AuthenticationPrincipal User user) {
         if (!result.hasErrors()) {
             commentService.saveOrUpdateComment(comment, user, id);
+            scoreService.voteOnComment(comment.getId(), user, RatingType.UPVOTE);
+
         }
         return "redirect:/reviews/review/" + id;
     }
