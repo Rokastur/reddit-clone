3e28b9febd9d57c6055eda96ef10300af97a44a9
Rokas Atminas <rokasatminas@gmail.com>
2020-12-31 12:02:25 +0200

    refactored SQL queries to use table aliases for better readability.

--- src/main/java/com/blog/reviewwebsite/repositories/ScoreRepository.java
+++ src/main/java/com/blog/reviewwebsite/repositories/ScoreRepository.java
@@ -8,28 +8,28 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ScoreRepository extends JpaRepository<Score, Long> {
 
-    @Query(value = "SELECT * FROM score JOIN review_score ON score.id = review_score.score_id WHERE user_id =:userId AND review_id =:reviewId", nativeQuery = true)
+    @Query(value = "SELECT * FROM score s JOIN review_score rs ON s.id = rs.score_id WHERE user_id =:userId AND review_id =:reviewId", nativeQuery = true)
     Score getOneByUserAndReview(Long userId, Long reviewId);
 
-    @Query(value = "SELECT COUNT(*) FROM score JOIN review_score ON score.id = review_score.score_id WHERE rating_type = 'UPVOTE' AND review_id =:reviewId", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM score s JOIN review_score rs ON s.id = rs.score_id WHERE rating_type = 'UPVOTE' AND review_id =:reviewId", nativeQuery = true)
     long getUpvoteCountByReview(Long reviewId);
 
-    @Query(value = "SELECT COUNT(*) FROM score JOIN review_score ON score.id = review_score.score_id WHERE rating_type = 'DOWNVOTE' AND review_id =:reviewId", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM score s JOIN review_score rs ON s.id = rs.score_id WHERE rating_type = 'DOWNVOTE' AND review_id =:reviewId", nativeQuery = true)
     long getDownvoteCountByReview(Long reviewId);
 
-    @Query(value = "SELECT * FROM score JOIN comment_score ON score.id = comment_score.score_id WHERE user_id =:userId and comment_id =:commentId", nativeQuery = true)
+    @Query(value = "SELECT * FROM score s JOIN comment_score cs ON s.id = cs.score_id WHERE user_id =:userId and comment_id =:commentId", nativeQuery = true)
     Score getOneByUserAndComment(Long userId, Long commentId);
 
-    @Query(value = "SELECT COUNT(*) FROM score JOIN comment_score ON score.id = comment_score.score_id WHERE rating_type = 'UPVOTE' AND comment_id =:commentId", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM score s JOIN comment_score cs ON s.id = cs.score_id WHERE rating_type = 'UPVOTE' AND comment_id =:commentId", nativeQuery = true)
     long getUpvoteCountByComment(Long commentId);
 
-    @Query(value = "SELECT COUNT(*) FROM score JOIN comment_score ON score.id = comment_score.score_id WHERE rating_type = 'DOWNVOTE' AND comment_id =:commentId", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM score s JOIN comment_score cs ON s.id = cs.score_id WHERE rating_type = 'DOWNVOTE' AND comment_id =:commentId", nativeQuery = true)
     long getDownvoteCountByComment(Long commentId);
 
-    @Query(value = "SELECT COUNT(*) FROM score JOIN review_score ON score.id = review_score.score_id WHERE rating_type = 'UPVOTE' AND review_id IN (SELECT review_id FROM review WHERE id =:userId)", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM score s JOIN review_score rs ON s.id = rs.score_id WHERE rating_type = 'UPVOTE' AND review_id IN (SELECT id FROM review WHERE user_id =:userId)", nativeQuery = true)
     long getHowManyTimesThisUsersPostsHaveBeenUpvoted(Long userId);
 
-    @Query(value = "SELECT COUNT(*) FROM score JOIN comment_score ON score.id = comment_score.score_id WHERE rating_type = 'UPVOTE' AND comment_id IN (SELECT comment_id FROM comments WHERE user_id =:userId)", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM score s JOIN comment_score cs ON s.id = cs.score_id WHERE rating_type = 'UPVOTE' AND comment_id IN (SELECT id FROM comments WHERE user_id =:userId)", nativeQuery = true)
     long getHowManyTimesThisUsersCommentsHaveBeenUpvoted(Long userId);
 
 
