5ecf92fc300fbbddeda95e906995ec0717ac491f
Rokas Atminas <rokasatminas@gmail.com>
2021-01-04 02:05:22 +0200

    fixed constraints - @MIn didn't make sense on a String.

--- src/main/java/com/blog/reviewwebsite/entities/UserDTO.java
+++ src/main/java/com/blog/reviewwebsite/entities/UserDTO.java
@@ -5,17 +5,18 @@ import lombok.Setter;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class UserDTO {
 
     @NotEmpty(message = "username must not be empty")
-    @Min(value = 3, message = "minimum length must be at least 3 characters")
+    @Size(min = 3, message = "minimum length must be at least 3 characters")
     private String username;
 
     @NotEmpty(message = "password must not be empty")
-    @Min(value = 3, message = "minimum length must be at least 3 characters")
+    @Size(min = 3, message = "minimum length must be at least 3 characters")
     private String password;
 
     private String confirmPassword;
