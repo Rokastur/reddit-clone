672aeeffa07a0016f1247c0e2f168fa4493c3b6a
Rokas Atminas <rokasatminas@gmail.com>
2021-01-05 04:40:06 +0200

    modified queries to include more information

--- src/main/java/com/blog/reviewwebsite/repositories/CategoryRepository.java
+++ src/main/java/com/blog/reviewwebsite/repositories/CategoryRepository.java
@@ -12,12 +12,12 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     String categoryCountQuery = "SELECT COUNT(*) FROM CATEGORY";
 
-    @Query(value = "SELECT c.id FROM category c LEFT JOIN review r ON c.id = r.category_id GROUP BY c.id ORDER BY COUNT(*) DESC",
+    @Query(value = "SELECT c.* FROM category c LEFT JOIN review r ON c.id = r.category_id GROUP BY c.id ORDER BY COUNT(*) DESC",
             countQuery = categoryCountQuery,
             nativeQuery = true)
     Page<Category> findAllOrderByPostCountDesc(Pageable pageable);
 
-    @Query(value = "SELECT c.id FROM category c LEFT JOIN review r ON c.id = r.category_id GROUP BY c.id ORDER BY COUNT(*) ASC",
+    @Query(value = "SELECT c.* FROM category c LEFT JOIN review r ON c.id = r.category_id GROUP BY c.id ORDER BY COUNT(*) ASC",
             countQuery = categoryCountQuery,
             nativeQuery = true)
     Page<Category> findAllOrderByPostCountAsc(Pageable pageable);
@@ -32,12 +32,12 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
             nativeQuery = true)
     Page<Category> findAllOrderByFollowersCountAsc(Pageable pageable);
 
-    @Query(value = "SELECT c.id, date FROM category c LEFT JOIN review r ON c.id = r.id ORDER BY DATE DESC NULLS LAST",
+    @Query(value = "SELECT c.*, date FROM category c LEFT JOIN review r ON c.id = r.id ORDER BY DATE DESC NULLS LAST",
             countQuery = categoryCountQuery,
             nativeQuery = true)
     Page<Category> findAllOrderByNewestPost(Pageable pageable);
 
-    @Query(value = "SELECT c.id, date FROM category c LEFT JOIN review r ON c.id = r.id ORDER BY DATE ASC NULLS LAST",
+    @Query(value = "SELECT c.*, date FROM category c LEFT JOIN review r ON c.id = r.id ORDER BY DATE ASC NULLS LAST",
             countQuery = categoryCountQuery,
             nativeQuery = true)
     Page<Category> findAllOrderByOldestPost(Pageable pageable);
