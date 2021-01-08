e60b9ae3bea203d026a2f323337fa157bf42272d
Rokas Atminas <rokasatminas@gmail.com>
2021-01-08 10:39:20 +0200

    added default value to fix error - without default value code throws null pointer exception

--- src/main/java/com/blog/reviewwebsite/entities/Score.java
+++ src/main/java/com/blog/reviewwebsite/entities/Score.java
@@ -18,7 +18,7 @@ public class Score {
 
     @Column(name = "rating_type")
     @Enumerated(EnumType.STRING)
-    private RatingType ratingType;
+    private RatingType ratingType = RatingType.NONE;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id")
