0c1589c97f618e6f9ed5c57ef26ce15374c79e08
Rokas Atminas <rokasatminas@gmail.com>
2021-01-08 23:50:31 +0200

    added methods to get entity reference and entity itself. use get reference method for when only entity id is needed (i.e assigning id in association)

--- src/main/java/com/blog/reviewwebsite/services/UserService.java
+++ src/main/java/com/blog/reviewwebsite/services/UserService.java
@@ -40,6 +40,14 @@ public class UserService implements UserDetailsService {
         return userRepository.getOne(id);
     }
 
+    public User getUserReference(Long id) {
+        return userRepository.getOne(id);
+    }
+
+    public User getUserEntity(Long id) {
+        return userRepository.findById(id).get();
+    }
+
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
